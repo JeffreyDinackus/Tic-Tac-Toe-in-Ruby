69393dfe2cf92a7e93656efb86159d9b8dfb12b8
JeffreyDinackus <jeffreydinackus@proton.me>
2024-01-05 09:53:32 -0500

    win checking partially added, now board resets after each game

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -21,18 +21,15 @@ class Game
       [[7],[8],[9]]]
 
   def initialize
-    @won = false
-
-      @current = @@empty
-      @current_piece = "O"
-      @other_piece = "X"
+    reset()
   end
 
   def run
     puts "welcome to TIC TAC TOE."
     puts "choose the corresponding spot number when it is your turn to place a piece. the current turn will be outputted."
           puts "if the piece isn't being placed, it's probably already been taken."
-
+      reset()
+      p @empty
     while !check() do
 
       show_board()
@@ -63,7 +60,7 @@ class Game
 
     end
 
-
+    puts "#{@current_winner} WINS. GAME OVER." # may need to be swapped for the other piece.
 
   end
 
@@ -80,13 +77,52 @@ puts "Match up the number with the spot you want to place your piece."
 
 
   protected
-  def check
-    return false
 
+
+  def reset
+
+      @won = false
+      @@empty = [
+
+
+      [[],[],[]],
+
+        [[],[],[]],
+      [[],[],[]]]
+      @current = @@empty
+      @current_piece = "O"
+      @other_piece = "X"
+      @current_winner = "NO WINNER"
+  end
+
+  def check
+    assertion = ["X","O"]
+    # not a bad way of doing it
+   assertion.each do |a|
+      (0..2).each do |denom|
+
+        if @current[denom][0] == a && @current[denom][1] == a && @current[denom][2] == a then
+          @current_winner = a
+          return true
+        end
+      end
+    end
+
+   assertion.each do |a|
+      (0..2).each do |denom|
+
+        if @current[denom][0] == a && @current[denom][1] == a && @current[denom][2] == a then
+          @current_winner = a
+          return true
+        end
+      end
+    end
+
+    return false
   end
   def turn(num)
 
-    p @current_piece
+
     # this sets the line number in the array for current. it is required to make the correct spot findable in the current array.
     denom = num > 6 ? 2 : ( num > 3 ? 1 : 0)
     numerator = num % 3 == 0 ? 2 : (num == 2 || num == 5 || num == 8 ? 1 : 0)
